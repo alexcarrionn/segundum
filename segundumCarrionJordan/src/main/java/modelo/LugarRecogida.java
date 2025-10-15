2d9b9c726bef7a883d8e571507d3cf8a51d21685
Alejandro <acarrionjordan@gmail.com>
2025-10-15 21:55:39 +0200

    creadas las clases principales junto a la anotacion JPA

--- segundumCarrionJordan/src/main/java/modelo/LugarRecogida.java
+++ segundumCarrionJordan/src/main/java/modelo/LugarRecogida.java
@@ -1,5 +1,61 @@
 package modelo;
 
-public class LugarRecogida {
+import javax.persistence.*;
 
+@Entity
+public class LugarRecogida {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE)
+	private String id;
+	
+	@Lob
+	private String descripcion;
+	
+	private double longitud;
+	
+	private double latitud;
+	
+	public LugarRecogida() {
+	}
+	
+	public LugarRecogida(String descripcion, double longitud, double latitud) {
+		this.descripcion = descripcion;
+		this.longitud = longitud;
+		this.latitud = latitud;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getDescripcion() {
+		return descripcion;
+	}
+	
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	public double getLongitud() {
+		return longitud;
+	}
+	
+	public void setLongitud(double longitud) {
+		this.longitud = longitud;
+	}
+	
+	public double getLatitud() {
+		return latitud;
+	}
+	
+	public void setLatitud(double latitud) {
+		this.latitud = latitud;
+	}
+	
+	
 }
--- segundumCarrionJordan/src/main/java/modelo/Producto.java
+++ segundumCarrionJordan/src/main/java/modelo/Producto.java
@@ -1,5 +1,150 @@
 package modelo;
 
-public class Producto {
+import java.time.LocalDateTime;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE)
+	private String id; 
+	
+	private String titulo; 
+	
+	@Lob
+	private String descripcion;
+	private double precio;
+	
+	@Enumerated(EnumType.STRING)
+	private EstadoProducto estado;
+	
+	private LocalDateTime fechaPublicacion;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "categoria_id")
+	private Categoria categoria;
+	
+	private int visualizaciones;
+	private boolean envioDisponible;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "lugar_recogida_id")
+	private LugarRecogida lugarRecogida;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "vendedor_id")
+	private Usuario vendedor; 
+	
+	public Producto() {
+	}
+	
+	public Producto(String titulo, String descripcion, double precio, EstadoProducto estado, LocalDateTime fechaPublicacion,
+			Categoria categoria, int visualizaciones, boolean envioDisponible, LugarRecogida lugarRecogida,
+			Usuario vendedor) {
+		this.titulo = titulo;
+		this.descripcion = descripcion;
+		this.precio = precio;
+		this.estado = estado;
+		this.fechaPublicacion = fechaPublicacion;
+		this.categoria = categoria;
+		this.visualizaciones = visualizaciones;
+		this.envioDisponible = envioDisponible;
+		this.lugarRecogida = lugarRecogida;
+		this.vendedor = vendedor;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public EstadoProducto getEstado() {
+		return estado;
+	}
+
+	public void setEstado(EstadoProducto estado) {
+		this.estado = estado;
+	}
+
+	public LocalDateTime getFechaPublicacion() {
+		return fechaPublicacion;
+	}
+
+	public void setFechaPublicacion(LocalDateTime fechaPublicacion) {
+		this.fechaPublicacion = fechaPublicacion;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public int getVisualizaciones() {
+		return visualizaciones;
+	}
+
+	public void setVisualizaciones(int visualizaciones) {
+		this.visualizaciones = visualizaciones;
+	}
+
+	public boolean isEnvioDisponible() {
+		return envioDisponible;
+	}
+
+	public void setEnvioDisponible(boolean envioDisponible) {
+		this.envioDisponible = envioDisponible;
+	}
+
+	public LugarRecogida getLugarRecogida() {
+		return lugarRecogida;
+	}
+
+	public void setLugarRecogida(LugarRecogida lugarRecogida) {
+		this.lugarRecogida = lugarRecogida;
+	}
+
+	public Usuario getVendedor() {
+		return vendedor;
+	}
+
+	public void setVendedor(Usuario vendedor) {
+		this.vendedor = vendedor;
+	}
+	
+	
+	
+	
 }
